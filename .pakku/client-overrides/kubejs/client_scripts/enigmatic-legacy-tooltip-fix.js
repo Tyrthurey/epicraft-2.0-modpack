f3cb6a29062f283b5aa2de4567b67031c6c80557
Cobwebblox <69752942+Cobwebblox@users.noreply.github.com>
2026-04-27 16:29:29 +0200

    fix enigmatic-legacy-tooltip-fix.js client script
    used to be an empty file

--- .pakku/client-overrides/kubejs/client_scripts/enigmatic-legacy-tooltip-fix.js
+++ .pakku/client-overrides/kubejs/client_scripts/enigmatic-legacy-tooltip-fix.js
@@ -1,3 +1,13 @@
-// Visit the wiki for more info - https://kubejs.com/
-console.info('Hello, World! (Loaded client example script)')
+ItemEvents.modifyTooltips(event => {
+  event.modify('enigmaticlegacyplus:etherium_scythe', tooltip => {
+    tooltip.removeLine(1)
+    tooltip.insert(1, Text.of('Tills blocks in a 3x3 area.').color(0xAA00AA))
+    tooltip.removeLine(2)
+    tooltip.insert(2, Text.of('Hold Shift to suppress this effect.').color(0xAA00AA))
+  })
 
+  event.modify('enigmaticlegacyplus:angel_blessing', {shift: true}, tooltip => {
+    tooltip.removeLine(4)
+    tooltip.insert(4, Text.of('- Accelerates you upwards.').color(0xFFAA00))
+  })
+})
